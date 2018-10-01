4a38f953604c9f0ac3c2afccc25830383b45037b
Infernocreator33 <zs0877310@otc.edu>
2018-10-01 17:23:44 -0500

    update to turn in

--- imageSwapTimed.js
+++ imageSwapTimed.js
@@ -1,17 +1,22 @@
+var images = ["Vader.jpg", "yoda.jpeg", "HanSolo.jpg"];
+var image = document.getElementById('Image');
+var index = 0;
 function myTimer()
 {
-    var images = ["Vader.jpg", "yoda.jpeg", "HanSolo.jpg"];
-    var image = document.getElementById('Image');
-    var index = 0;
     function changeImage()
     {
         image.src = images[index];
         index++;
-        if(index == images.length)
+        if(index >= images.length)
         {
             index = 0;
         }
     }
     setInterval(changeImage, 3000);
+}
+
+function addOnClick(index)
+{
+    image.src = index;
 }
 window.addEventListener("load", myTimer);
